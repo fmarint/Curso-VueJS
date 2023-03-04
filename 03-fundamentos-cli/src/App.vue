b34bc2f50557d83be1010c80cd36ebfe2ec4cce2
fmarint <fmarin.devnet@gmail.com>
2023-03-04 18:06:07 -0500

    counter terminado

---
+++ 03-fundamentos-cli/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Counter from './components/Counter.vue';
+// import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+      <Counter :start="15" title="Contador" />
+  </header>
+</template>
+
+<style scoped>
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
--- 03-fundamentos-cli/src/components/Counter.vue
+++ 03-fundamentos-cli/src/components/Counter.vue
@@ -1,21 +1,23 @@
-<template>
-    <h2>{{ title }}</h2>
-    <p>{{ counter }}<sup>2</sup> = {{ counterPow }}</p>
-    <hr />
-    <div>
+<template >
+    <div class="centrar">
+        <h1>{{ title }}</h1>
+        <p>{{ counter }}<sup>2</sup> = {{ counterPow }}</p>
+    </div>
+    <!-- <hr /> -->
+    <div class="centrar">
         <button type="button" @click="counterHandler('-')">-1</button>
         <button type="button" v-on:click="() => counterHandler('+')">+1</button>
     </div>
 </template>
 <script>
- 
+
 export default {
-    props: { 
+    props: {
         title: String,
         start: {
             type: Number,
             requerid: true,
-            default :5,
+            default: 5,
             // validator: (val) => {
             //     return val>=0?true:false
             // }
@@ -43,13 +45,20 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+.centrar {
+    text-align: center;
+}
+p{
+    font-weight: bold;
+    font-size: 18px;
+}
 button {
     margin-right: 10px;
     color: rgb(43, 41, 41);
     background-color: rgb(134, 184, 59);
     border: thin;
-    border-radius: 20%;
+    border-radius: 50%;
     cursor: pointer;
     font-weight: bold;
     padding: 4px 8px;
